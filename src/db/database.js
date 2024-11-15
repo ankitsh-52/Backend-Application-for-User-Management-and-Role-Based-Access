5b678d5c1f7579c1b26c60b384841b7ee7bea590
Ankit Kumar Sharma <ankitkrsharma5595@gmail.com>
2024-11-15 22:23:14 +0530

    Just fixing some bugs

--- src/db/database.js
+++ src/db/database.js
@@ -14,13 +14,16 @@ const connectDB = async () => {
         TODO : 2.)console.log(`Mongo Db Connected!! ${connectionDB.connection.host}`);
         TODO 3.) process.exit(1)
          */
-        const connectionDB = await mongoose.connect(`${process.env.MONGODB_URI}`, {
-            maxPoolSize : 10,
-            minPoolSize: 5,
-        });
+        // const connectionDB = await mongoose.connect(`${process.env.MONGODB_URI}`, {
+        //     maxPoolSize : 10,
+        //     minPoolSize: 5,
+        // });
         // const connectionDB = await mongoose.connect(`${process.env.COMPASS_URI}`)
-        // const connectionDB = await mongoose.connect(`${process.env.ATLASDB_URI}`)
-        console.log(`Mongo Db Connected!! ${connectionDB.connection.host}`);
+        const connectionDB = await mongoose.connect(`${process.env.ATLASDB_URI}`, {
+            minPoolSize: 5,
+            maxPoolSize : 10,
+        })
+        console.log(`MongoDB atlas Connected!! ${connectionDB.connection.host}`);
     } catch (error) {
         console.log("MONGODB connection error ", error);
         process.exit(1)
